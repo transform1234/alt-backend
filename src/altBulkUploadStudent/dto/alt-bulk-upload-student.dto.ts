0c2eee94f72ed29db0487d7ea29c8fb6aff680ff
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-03-11 14:16:45 +0530

    chore : added validations to API

--- src/altBulkUploadStudent/dto/alt-bulk-upload-student.dto.ts
+++ src/altBulkUploadStudent/dto/alt-bulk-upload-student.dto.ts
@@ -1,33 +1,25 @@
-import { Exclude, Expose } from "class-transformer";
+import { Exclude, Expose, Type } from "class-transformer";
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { StudentDto } from "src/altStudent/dto/alt-student.dto";
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsObject,
+  ValidateNested,
+} from "class-validator";
 
 export class ALTBulkUploadStudentDto {
-  // @ApiProperty({
-  //   type: String,
-  //   description: "School Udise",
-  // })
-  // @Expose()
-  // schoolUdise: string;
-
-  // @ApiProperty({
-  //   type: String,
-  //   description: "Group Id",
-  // })
-  // @Expose()
-  // groupId: string;
-
-  // @ApiProperty({
-  //   type: String,
-  //   description: "Password",
-  // })
-  // @Expose()
-  // password: string;
-
   @ApiProperty({
     type: [StudentDto],
     description: "Student List",
   })
+  @IsArray()
+  @IsObject({ each: true })
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(120)
+  @Type(() => StudentDto)
   @Expose()
   students: StudentDto[];
 }
